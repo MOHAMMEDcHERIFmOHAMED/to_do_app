d70dc97a0adfcc1c6513a55ec764535db91c0e3d
MOHAMMEDcHERIFmOHAMED <mafsamarari@gmail.com>
2024-06-29 15:25:31 +0200

    Update to_do_app.js
    The feature of saving data in internal storage has been added

--- to_do_app.js
+++ to_do_app.js
@@ -9,20 +9,49 @@ addBtn.onclick = addToList;
 let tasks_count = 0;
 let complete_count = 0;
 
+// Load tasks from local storage
+let arr = JSON.parse(localStorage.getItem("TASKS")) || [];
+loadData();
+
+// Function to load data from local storage
+function loadData() {
+    tasks_count = arr.length;
+    updateTaskCount();
+    for (task of arr) {
+        let table = `
+        <div data-id="${task.ID}" class="mession ${task.complet ? 'completed' : ''}">
+            <p p-id="${task.ID}">${task.textvalue}</p>
+            <button data-id="${task.ID}" class="rmBtn">delete</button>
+        </div>
+        `;
+        toDoContainer.innerHTML += table;
+        if (task.complet) {
+            complete_count++;
+        }
+    }
+    updateCompletedTaskCount();
+}
+
 // Add tasks to list
 function addToList() {
     let textvalue = input.value;
-    let ID = Date.now();
-    let table = `
-    <div data-id="${ID}" class="mession">
-        <p>${textvalue}</p>
-        <button data-id="${ID}" class="rmBtn">delete</button>
-    </div>
-    `;
-    tasks_count++;
-    updateTaskCount();
-    toDoContainer.innerHTML += table;
-    input.value = '';
+    let data;
+    if (textvalue != "") {
+        let ID = Date.now();
+        let table = `
+        <div data-id="${ID}" class="mession">
+            <p p-id="${ID}">${textvalue}</p>
+            <button data-id="${ID}" class="rmBtn">delete</button>
+        </div>
+        `;
+        tasks_count++;
+        updateTaskCount();
+        toDoContainer.innerHTML += table;
+        input.value = '';
+        data = { ID, textvalue, complet: false };
+        arr.push(data);
+        localStorage.setItem("TASKS", JSON.stringify(arr));
+    }
 }
 
 // Update tasks number
@@ -35,8 +64,8 @@ function updateCompletedTaskCount() {
     completeNBR.innerHTML = `${complete_count}`;
 }
 
-// Missions done
-toDoContainer.addEventListener('click', function(event) {
+// Event listener for marking tasks as done or deleting them
+toDoContainer.addEventListener('click', function (event) {
     if (event.target.tagName === 'P') {
         let ID = event.target.parentElement.getAttribute('data-id');
         doneMession(ID);
@@ -51,27 +80,37 @@ function deleteTask(ID) {
     let taskElement = document.querySelector(`[data-id="${ID}"]`);
     if (taskElement) {
         if (taskElement.classList.contains('completed')) {
-            complete_count--; 
-            completeNBR.innerHTML = `${complete_count}`;
+            complete_count--;
+            updateCompletedTaskCount();
         }
         taskElement.remove();
         tasks_count--;
-        updateTaskCount(); 
+        updateTaskCount();
+        arr = arr.filter(task => task.ID != ID);
+        localStorage.setItem("TASKS", JSON.stringify(arr));
     }
 }
 
-// Mark mission as done
+// Mark task as done
 function doneMession(ID) {
     let taskElement = document.querySelector(`[data-id="${ID}"]`);
-
     if (taskElement.classList.contains('completed')) {
+        for (let task of arr) {
+            if (task.ID == ID) {
+                task.complet = false;
+            }
+        }
         taskElement.classList.remove('completed');
         complete_count--;
-        completeNBR.innerHTML = `${complete_count}`;
     } else {
+        for (let task of arr) {
+            if (task.ID == ID) {
+                task.complet = true;
+            }
+        }
         taskElement.classList.add('completed');
         complete_count++;
-      completeNBR.innerHTML = `${complete_count}`;
     }
+    updateCompletedTaskCount();
+    localStorage.setItem("TASKS", JSON.stringify(arr));
 }
-
